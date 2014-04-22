fd5e3586eb4c446e8c7233bf5383339038c678e8
Dersu <dersugarciasanz@gmail.com>
2014-04-22 13:27:14 +0200

    Array de numeros es capicua?

--- EjerciciosJava/src/com/dersugarcia/arrays/Ej16_01.java
+++ EjerciciosJava/src/com/dersugarcia/arrays/Ej16_01.java
@@ -23,7 +23,7 @@ public class Ej16_01 {
 	
 	public static int max(int[] array) {
 		int max = array[0];
-		
+		// max = Integer.MIN_VALUE;
 		for(int i=1; i<array.length; i++) {
 			if (array[i]>max) {
 				max = array[i];
@@ -34,7 +34,7 @@ public class Ej16_01 {
 	}
 	public static int min(int[] array) {
 		int min = array[0];
-		
+		// min = Integer.MAX_VALUE;
 		for(int i=1; i<array.length; i++) {
 			if (array[i]<min) {
 				min = array[i];
---
+++ EjerciciosJava/src/com/dersugarcia/arrays/Ej17_01.java
@@ -0,0 +1,43 @@
+package com.dersugarcia.arrays;
+
+public class Ej17_01 {
+
+
+	public static void main(String[] args) {
+
+		int array[] = arrayNumerosAleatorios();
+		
+		if (esCapicua(array)) {
+			System.out.println("Es capicua");
+		} else {
+			System.out.println("No es capicua");
+		}
+		
+	}
+
+	public static int[] arrayNumerosAleatorios() {
+		
+		int array[] = new int[20];
+		
+		for (int i=0; i<20; i++) {
+			array[i] = (int) (100*Math.random()+1);
+		}
+//		int array[] = {1,2,3,4,5,6,7,8,9,0,0,9,8,7,6,5,4,3,2,1};
+		
+		return array;
+	}
+	
+	public static boolean esCapicua(int array[]){
+		
+		boolean bool = true;
+		int len = array.length;
+		int i=0;
+		while ( bool && i<len/2) {
+			bool = (array[i]==array[len-i-1]);
+			i++;
+		}
+		
+		
+		return bool;
+	}
+}
