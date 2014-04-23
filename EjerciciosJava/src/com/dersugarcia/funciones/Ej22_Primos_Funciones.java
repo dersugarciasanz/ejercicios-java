86b373c26be3bf83d57ab34adefa2bd5e7ba166f
Dersu <dersugarciasanz@gmail.com>
2014-04-23 10:16:41 +0200

    Primos iteración 3

--- EjerciciosJava/src/com/dersugarcia/funciones/Ej22_Primos_Funciones.java
+++ EjerciciosJava/src/com/dersugarcia/funciones/Ej22_Primos_Funciones.java
@@ -3,23 +3,9 @@ package com.dersugarcia.funciones;
 import java.util.Vector;
 
 
-public class Ej22_Primos_Funciones {
-	
-	private int numero;
-	
-	//Constructor
-	public Ej22_Primos_Funciones(int numero) {
-		this.numero = numero;
-	}
-	//Llamada a metodos primosHasta y esPrimo usando la variable de instancia
-	public Vector<Integer> primosHasta() {
-		return primosHasta(numero);
-	}
-	public boolean esPrimo() {
-		return esPrimo(numero);
-	}
-	
-	public Vector<Integer> primosHasta(int num) {
+public abstract class Ej22_Primos_Funciones {
+
+	public static Vector<Integer> primosHasta(int num) {
 		Vector<Integer> primos = new Vector<Integer>();
 
 		for (int i = 1; i <= num; i++) {
@@ -31,7 +17,7 @@ public class Ej22_Primos_Funciones {
 		return primos;
 	}
 
-	public boolean esPrimo(int num) {
+	public static boolean esPrimo(int num) {
 		int resto = 1;
 		int i = 2;
 		while (i < num && resto != 0) {
--- EjerciciosJava/src/com/dersugarcia/funciones/Ej22_Primos_Main.java
+++ EjerciciosJava/src/com/dersugarcia/funciones/Ej22_Primos_Main.java
@@ -1,21 +1,18 @@
 package com.dersugarcia.funciones;
 
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Ej22_Primos_Main {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		int num;
-//		Ej22_Primos_Funciones funciones = new Ej22_Primos_Funciones();
 		
 		System.out.println("Introduce un nœmero positivo para saber si es primo o 0 para salir:");
 		num=entrada.nextInt();
-		Ej22_Primos_Funciones funciones = new Ej22_Primos_Funciones(num);
 		while(num!=0) {
-			System.out.println(num + " es primo? " + funciones.esPrimo());
-			System.out.println("Numeros primos entre 1 y " + num + ": " + funciones.primosHasta().toString());
+			System.out.println(num + " es primo? " + Ej22_Primos_Funciones.esPrimo(num));
+			System.out.println("Numeros primos entre 1 y " + num + ": " + Ej22_Primos_Funciones.primosHasta(num).toString());
 			System.out.println("\nIntroduce un nœmero positivo para saber si es primo o 0 para salir:");
 			num=entrada.nextInt();
 		}
