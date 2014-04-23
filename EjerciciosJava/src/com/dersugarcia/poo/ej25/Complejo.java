70b2a936188ddcfa26c904f2457cce245450c5ed
Dersu <dersugarciasanz@gmail.com>
2014-04-23 11:55:57 +0200

    Ejercicio números complejos en java

---
+++ EjerciciosJava/src/com/dersugarcia/poo/ej25/Complejo.java
@@ -0,0 +1,43 @@
+package com.dersugarcia.poo.ej25;
+
+public class Complejo {
+
+	//Variables de instancia
+	private double pReal, pImag;
+	
+	
+	//Constructores
+	public Complejo() {
+		this.pReal = 0.0;
+		this.pImag = 0.0;
+	}
+	public Complejo(double pReal, double pImag) {
+		this.pReal = pReal;
+		this.pImag = pImag;
+	}
+
+	//MŽtodos de instancia
+	public void asignar(double x, double y) {
+		this.pReal = x;
+		this.pImag = y;
+	}
+	
+	public void sumar(Complejo b) {
+		this.pReal += b.pReal;
+		this.pImag += b.pImag;
+	}
+	
+	public String toString() {
+		return "(" + this.pReal + ", " + this.pImag + ")";
+	}
+	
+	
+	//MŽtodos de clase
+	public static Complejo sumar(Complejo a, Complejo b) {
+		Complejo c = new Complejo();
+		c.pReal = a.pReal + b.pReal;
+		c.pImag = a.pImag + b.pImag;
+		return c;
+	}
+	
+}
---
+++ EjerciciosJava/src/com/dersugarcia/poo/ej25/PruebaComplejo.java
@@ -0,0 +1,23 @@
+package com.dersugarcia.poo.ej25;
+
+public class PruebaComplejo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Complejo c1, c2, c3;
+		
+		c1 = new Complejo(1.0,7.1);
+		System.out.println("c1: " + c1.toString());
+		c2 = new Complejo();
+		System.out.println("c2: " + c2.toString());
+		c2.asignar(2.2, 3.5);
+		System.out.println("c2: " + c2.toString());
+		c3 = Complejo.sumar(c1,c2);
+		System.out.println("c3: " + c3.toString());
+		c3.sumar(c3);
+		System.out.println("c3: " + c3.toString());
+	}
+
+}
