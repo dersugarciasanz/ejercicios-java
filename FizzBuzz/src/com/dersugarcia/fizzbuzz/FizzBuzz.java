22c7b1786f03ff045bcd265763830ed46ac00775
Dersu <dersugarciasanz@gmail.com>
2014-04-24 10:43:02 +0200

    FizzBuzz primera operación

---
+++ FizzBuzz/src/com/dersugarcia/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.dersugarcia.fizzbuzz;
+
+public class FizzBuzz {
+
+
+	
+	public String calcular(int num) {
+		String s = "";
+		for(int i=1; i<=num; i++) {
+			if(i!=1) {
+				s += " ";
+			}
+			if(i%15 == 0){
+				s+="FizzBuzz";
+			} else if(i%3 == 0){
+				s+="Fizz";
+			} else if (i%5 == 0) {
+				s+="Buzz";
+			} else {
+				s += i;
+			}
+		
+		}
+		return s;
+	}
+	
+	
+}
