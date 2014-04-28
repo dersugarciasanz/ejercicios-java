7ffcfe444903ebacd339196f70abc61c95b73c78
Dersu <dersugarciasanz@gmail.com>
2014-04-28 09:02:37 +0200

    Test suite

---
+++ EmpresaEmpleado/src/com/dersugarcia/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.dersugarcia.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EmpleadoTest.class, EmpresaTest.class })
+public class AllTests {
+
+}
