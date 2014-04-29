d70e16bc136d7cb46a4121e62b66a312344a4b38
Dersu <dersugarciasanz@gmail.com>
2014-04-29 09:44:30 +0200

    JuegoException

---
+++ Practica/src/juegos/excepciones/JuegoException.java
@@ -0,0 +1,14 @@
+package juegos.excepciones;
+
+public class JuegoException extends Exception {
+
+	private static final long serialVersionUID = 2174759194159054777L;
+
+	public JuegoException(String arg0) {
+		
+		super(arg0);
+	}
+	
+	
+
+}
