2fb6010c990c9c0855e7063e53ca0dd1b33a51d9
Dersu <dersugarciasanz@gmail.com>
2014-05-01 20:03:40 +0200

    Cerrar escaner en finally

--- Practica/src/profesor/Teclado.java
+++ Practica/src/profesor/Teclado.java
@@ -6,23 +6,24 @@ import juegos.excepciones.JuegoException;
 
 public abstract class Teclado {
 	
-
-	
 	public static int leeEntero() throws JuegoException {
 		Scanner entrada = new Scanner(System.in);
 		int numero = 0;
 
-			try {
-				
-				numero = Integer.parseInt(entrada.next());
-			}catch(NumberFormatException  e) {
-				
-				throw new JuegoException("Debe introducir un nœmero v‡lido.");
-			} catch(Exception ex) {
-				
-				numero = 0;
-			}
+		try {
 
+			numero = Integer.parseInt(entrada.next());
+		} catch (NumberFormatException e) {
+
+			throw new JuegoException("Debe introducir un nï¿½mero vï¿½lido.");
+		} catch (Exception ex) {
+
+			numero = 0;
+		} finally {
+			
+			entrada.close();
+		}
+		
 		return numero;
 	}
 }
